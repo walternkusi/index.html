79b28f520e0b59148c7d710002a275afd9cd9175
walter <150445786+walternkusi@users.noreply.github.com>
2024-04-20 00:08:08 +0200

    Add files via upload

---
+++ portfolio/index.html/2d.js
@@ -0,0 +1,50 @@
+alert("hello friend")
+const Name = document.getElementById('name')
+
+const Email = document.getElementById('email')
+
+const Message = document.getElementById('message')
+
+const form = document.getElementById('form')
+
+const name_error = document.getElementById('name_error')
+
+const Email_error = document.getElementById('email_error')
+
+const Message_error = document.getElementById('message_error')
+
+form.addEventListener('submit',(e)=> 
+{
+
+   if(Name.value === '' || Name.value == null)
+   {
+    e.preventDefault(); 
+    name_error.innerHTML= "name is required";
+    }
+else
+{
+   name_error.innerHTML= "";
+}
+
+if (Email.value=== '' || Name.value == null)
+{
+   e.preventDefault(); 
+   Email_error.innerHTML= "email is required";
+   }
+   else
+   {
+      Email_error.innerHTML= "";  
+   }
+
+   if(Message.value=== '' || Message.value == null)
+   {
+      e.preventDefault(); 
+      Message_error.innerHTML= "message is required";
+      }
+   else
+   {
+   Message_error.innerHTML= ""; 
+   }
+   
+   
+})
